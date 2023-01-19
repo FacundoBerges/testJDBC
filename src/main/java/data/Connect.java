de2bd6667267b132a25dcdadd4b61f793cf59f40
Facundo Berges <facundo.h.berges@gmail.com>
2023-01-19 14:31:19 -0300

    Connection pool apache

--- src/main/java/data/Connect.java
+++ src/main/java/data/Connect.java
@@ -1,6 +1,8 @@
 package data;
 
 import java.sql.*;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 /**
  * @author Facundo
@@ -10,11 +12,26 @@ public class Connect
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASSWD = "admin";
+    private static BasicDataSource dataSource = null;
     
+    
+    public static DataSource getDataSource()
+    {
+        if(Connect.dataSource == null)
+        {
+            Connect.dataSource = new BasicDataSource();
+            Connect.dataSource.setUrl(JDBC_URL);
+            Connect.dataSource.setUsername(JDBC_USER);
+            Connect.dataSource.setPassword(JDBC_PASSWD);
+            Connect.dataSource.setInitialSize(5);
+        }
+        
+        return Connect.dataSource;
+    }
     
     public static Connection getConnection() throws SQLException
     {
-        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWD);
+        return Connect.getDataSource().getConnection();
     }
     
     public static void close(ResultSet rs) throws SQLException
